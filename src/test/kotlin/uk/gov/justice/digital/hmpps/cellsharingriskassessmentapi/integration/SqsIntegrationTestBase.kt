9518147b1d9314b98226a241fd5a0ec19832cb67
Dafydd Houston <159147179+dafhoustonmoj@users.noreply.github.com>
2025-11-05 10:14:38 +0000

    Map-2958: initial project setup (#2)
    * MAP-2958: Initial project config and kotlin setup
    
    * MAP-2958: Initial project config and kotlin setup
    
    * MAP-2958: address audit review comment

---
+++ src/test/kotlin/uk/gov/justice/digital/hmpps/cellsharingriskassessmentapi/integration/SqsIntegrationTestBase.kt
@@ -0,0 +1,78 @@
+package uk.gov.justice.digital.hmpps.cellsharingriskassessmentapi.integration
+
+import org.awaitility.kotlin.await
+import org.awaitility.kotlin.matches
+import org.awaitility.kotlin.untilCallTo
+import org.junit.jupiter.api.BeforeEach
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.test.context.ActiveProfiles
+import org.springframework.test.context.DynamicPropertyRegistry
+import org.springframework.test.context.DynamicPropertySource
+import software.amazon.awssdk.services.sqs.model.PurgeQueueRequest
+import software.amazon.awssdk.services.sqs.model.ReceiveMessageRequest
+import uk.gov.justice.digital.hmpps.cellsharingriskassessmentapi.config.LocalStackContainer
+import uk.gov.justice.digital.hmpps.cellsharingriskassessmentapi.config.LocalStackContainer.setLocalStackProperties
+import uk.gov.justice.digital.hmpps.cellsharingriskassessmentapi.service.HMPPSDomainEvent
+import uk.gov.justice.hmpps.sqs.HmppsQueue
+import uk.gov.justice.hmpps.sqs.HmppsQueueService
+import uk.gov.justice.hmpps.sqs.countMessagesOnQueue
+import kotlin.also
+import kotlin.collections.map
+import kotlin.jvm.java
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@ActiveProfiles("test")
+class SqsIntegrationTestBase : IntegrationTestBase() {
+
+  @Autowired
+  private lateinit var hmppsQueueService: HmppsQueueService
+
+  private val auditQueue by lazy { hmppsQueueService.findByQueueId("audit") as HmppsQueue }
+  private val testDomainEventQueue by lazy { hmppsQueueService.findByQueueId("test") as HmppsQueue }
+
+  @BeforeEach
+  fun cleanQueue() {
+    auditQueue.sqsClient.purgeQueue(PurgeQueueRequest.builder().queueUrl(auditQueue.queueUrl).build())
+    testDomainEventQueue.sqsClient.purgeQueue(PurgeQueueRequest.builder().queueUrl(testDomainEventQueue.queueUrl).build())
+    auditQueue.sqsClient.countMessagesOnQueue(auditQueue.queueUrl).get()
+    testDomainEventQueue.sqsClient.countMessagesOnQueue(testDomainEventQueue.queueUrl).get()
+  }
+
+  companion object {
+    private val localStackContainer = LocalStackContainer.instance
+
+    @Suppress("unused")
+    @JvmStatic
+    @DynamicPropertySource
+    fun testcontainers(registry: DynamicPropertyRegistry) {
+      localStackContainer?.also { setLocalStackProperties(it, registry) }
+    }
+  }
+
+  fun getNumberOfMessagesCurrentlyOnQueue(): Int = testDomainEventQueue.sqsClient.countMessagesOnQueue(testDomainEventQueue.queueUrl).get()
+
+  fun getDomainEvents(messageCount: Int = 1): List<HMPPSDomainEvent> {
+    val sqsClient = testDomainEventQueue.sqsClient
+
+    val messages: MutableList<HMPPSDomainEvent> = mutableListOf()
+    await untilCallTo {
+      messages.addAll(
+        sqsClient.receiveMessage(ReceiveMessageRequest.builder().queueUrl(testDomainEventQueue.queueUrl).build())
+          .get()
+          .messages()
+          .map { objectMapper.readValue(it.body(), HMPPSMessage::class.java) }
+          .map { objectMapper.readValue(it.Message, HMPPSDomainEvent::class.java) },
+      )
+    } matches { messages.size == messageCount }
+
+    return messages
+  }
+}
+
+data class HMPPSEventType(val Value: String, val Type: String)
+data class HMPPSMessageAttributes(val eventType: HMPPSEventType)
+data class HMPPSMessage(
+  val Message: String,
+  val MessageAttributes: HMPPSMessageAttributes,
+)
